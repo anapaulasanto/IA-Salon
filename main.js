4e794e322f7d50559b88a7108dc9899b4613f9fa
Ana paula <149811410+anapaulasanto@users.noreply.github.com>
2023-11-25 14:57:19 -0300

    att

--- main.js
+++ main.js
@@ -4,3 +4,24 @@ var menuBar = document.querySelector('nav ul')
 iconBar.addEventListener("click",function(e) {
     menuBar.classList.toggle('hide');
 });
+
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        center: true,
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoWidth: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+});
